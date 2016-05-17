4c0a607c051d41e0bf333e5f47ceb6b2a96dfe2d
x-pengg <710843409@qq.com>
2016-05-17 12:36:53 +0800

    fk

--- src/setting/index.js
+++ src/setting/index.js
@@ -1,7 +1,7 @@
 export default  {
     blogTitle: 'UnKnown Me',
     config: {
-        repo: 'viko16/vue-ghpages-blog',
+        repo: 'x-pengg/vue-ghpages-blog',
         path: 'markdown',
         branch: 'markdown'
     }
