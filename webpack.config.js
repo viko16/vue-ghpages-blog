3bb61c640c6e9980b3de5c95d9b65912c9adb1fa
viko16 <16viko@gmail.com>
2016-10-08 01:28:57 +0800

    chore: setup webpack and extract single css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 var path = require('path')
 var webpack = require('webpack')
 var HtmlWebpackPlguin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+var pkg = require('./package.json')
+var banner =
+  pkg.description +
+  '\n' + "v" + pkg.version + ' (c)' + new Date().getFullYear() + " " + pkg.author +
+  '\n' + pkg.homepage
 
 module.exports = {
   entry: './src/main.js',
@@ -11,6 +18,11 @@ module.exports = {
   },
   resolveLoader: {
     root: path.join(__dirname, 'node_modules')
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
   },
   module: {
     loaders: [
@@ -34,14 +46,33 @@ module.exports = {
           name: '[name].[ext]?[hash]'
         }
       }
+    ],
+    noParse: [
+      /\.min\.js/
     ]
   },
   plugins: [
+    new webpack.LoaderOptionsPlugin({
+      vue: {
+        loaders: {
+          stylus: ExtractTextPlugin.extract({
+            loader: 'css?{discardComments:{removeAll:true}}!stylus',
+            fallbackLoader: 'vue-style-loader'
+          })
+          // stylus: 'vue-style!css?{discardComments:{removeAll:true}}!stylus'
+        }
+      }
+    }),
     new HtmlWebpackPlguin({
-      title: 'test',
       template: path.resolve(__dirname, 'src', 'tpl.html'),
-      filename: 'index.html'
-    })
+      filename: 'index.html',
+      minify: {
+        // https://github.com/kangax/html-minifier#options-quick-reference
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+    new ExtractTextPlugin('build.css')
   ],
   devServer: {
     historyApiFallback: true,
@@ -50,17 +81,11 @@ module.exports = {
     contentBase: 'dist/',
     host: '0.0.0.0'
   },
-  devtool: '#eval-source-map',
-  vue: {
-    loaders: {
-      css: 'vue-style!css',
-      stylus: 'vue-style!css!stylus'
-    }
-  }
+  devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = false
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
@@ -71,7 +96,11 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
+      },
+      output: {
+        comments: false
       }
-    })
+    }),
+    new webpack.BannerPlugin(banner)
   ])
 }
