dee72967febbd3848226f750bc7b80eb48ce9b1f
viko16 <16viko@gmail.com>
2016-11-03 14:14:45 +0800

    test: add App test

---
+++ test/unit/specs/App.spec.js
@@ -0,0 +1,28 @@
+'use strict'
+
+import Vue from 'vue'
+import App from '../../../src/App.vue'
+import router from '../../../src/router'
+import conf from '../../../src/conf.json'
+
+import '../../../src/utils/filter'
+
+describe('App.vue', function () {
+  it('should mount correctly', function () {
+    const app = new Vue(
+      Vue.util.extend({ router }, App)
+    ).$mount()
+    expect(app.$route).to.be.a('object')
+    expect(app.$children).not.to.be.empty
+    // methods
+    expect(app.resetSearch).to.be.a('function')
+    expect(app.selectSearchText).to.be.a('function')
+  })
+
+  it('should have correct data', function () {
+    const correctTitle = conf.blogTitle
+    expect(App.data).to.be.a('function')
+    expect(App.data().title).to.equal(correctTitle)
+    expect(App.data().keyword).to.be.empty
+  })
+})
