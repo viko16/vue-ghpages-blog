05a484145b58a7f9a52ec2d55c5e88ded5304da2
viko16 <16viko@gmail.com>
2016-11-06 21:33:02 +0800

    fix: disable devtool in production builds. fixed #13.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import App from './App.vue'
 
 import './utils/filter'
 
+// whether to allow vue-devtools inspection
+// false in production builds
+Vue.config.devtools = process.env.NODE_ENV !== 'production'
+
 const app = new Vue(
   Vue.util.extend({ router }, App)
 ).$mount('#app')
